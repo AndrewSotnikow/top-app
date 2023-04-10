b6bd5e4a639d632974a6e318a94fbb6e760bcf22
AndriiSotnikov <andrii.sotnikov@wtt-solution.com>
2023-04-10 19:07:27 +0200

    add button component

---
+++ components/Button/Button.props.ts
@@ -0,0 +1,11 @@
+import { DetailedHTMLProps, ButtonHTMLAttributes, ReactNode } from 'react'
+
+export interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  children: ReactNode
+  appearance: 'primary' | 'ghost'
+  arrow?: 'right' | 'down' | 'none'
+}
