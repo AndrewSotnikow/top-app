16b0d0ebc963709f722da7c6fd8bb7fecf3ab964
AndriiSotnikov <andrii.sotnikov@wtt-solution.com>
2023-04-14 18:24:34 +0200

    style footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,8 +1,14 @@
 import { HeaderProps } from './Footer.props'
 import cn from 'classnames'
 
-import styles from './Header.module.css'
+import styles from './Footer.module.css'
 
-export const Footer = ({ ...props }: HeaderProps): JSX.Element => {
-  return <div {...props}>Header</div>
+import { format } from 'date-fns'
+
+export const Footer = ({ className, ...props }: HeaderProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      OwlTop © 2022 - {format(new Date(), 'yyyy')} All rights reserved
+    </footer>
+  )
 }
