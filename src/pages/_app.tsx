332a40e62d485845d4970a0a702221251b7905fb
AndrewSotnikov <sot.shot@gmail.com>
2023-04-30 12:16:23 +0200

    add meta info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,30 @@
 import type { AppProps } from 'next/app'
 import { Noto_Sans } from 'next/font/google'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 const notoSans = Noto_Sans({
   weight: ['300', '400', '500', '600'],
   subsets: ['latin'],
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <main className={notoSans.className}>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
+        <meta
+          property='og:url'
+          content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+        />
+        <meta property='og:locale' content='ru_RU' />
+      </Head>
       <Component {...pageProps} />
     </main>
   )
